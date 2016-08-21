61f62365a91127329adc233c591733e6fb284c93
AllenFang <ayu780129@hotmail.com>
2016-08-21 23:43:05 +0800

    home

--- src/routers/app.jsx
+++ src/routers/app.jsx
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react';
 import { Provider } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
+import Title from '../components/title';
 import Header from '../components/shared/header';
 import Footer from '../components/shared/footer';
 import timerStore from '../stores/timer';
@@ -17,8 +18,9 @@ export default class AppRoute extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <main className='wrap' role='main'>
-          <Header />
+          <Title />
           <Provider { ...{ timerStore } }>
             { this.props.children }
           </Provider>
--- src/routers/home.jsx
+++ src/routers/home.jsx
@@ -1,39 +1,15 @@
 /* eslint react/prefer-stateless-function: 0 */
 import React, { Component } from 'react';
+import Project from '../components/project';
+import Info from '../components/info';
 
 class HomeRoute extends Component {
 
   render() {
     return (
       <div>
-        <section>
-          <h3>About</h3>
-          <ul>
-            <li>
-              <b>mobx-react-boilerplate</b> is really easy React and Mobx
-              boilerplate constructed with following:
-            </li>
-            <li><strong>react</strong></li>
-            <li><strong>mobx</strong></li>
-            <li><strong>react-router</strong></li>
-            <li><strong>eslint</strong></li>
-            <li><strong>webpack</strong></li>
-            <li><strong>ES6</strong></li>
-          </ul>
-        </section>
-        <section className='buttons'>
-          <h3>Download</h3>
-          <div className='download'>
-            <a className='button' href='https://github.com/iandevlin/html5bones/archive/v1.7.1.zip'>
-              Download <span className='version'>v1.7.1</span>
-            </a>
-          </div>
-          <div className='github'>
-            <a className='button' href='https://github.com/iandevlin/html5bones'>
-              GitHub Source
-            </a>
-          </div>
-        </section>
+        <Project />
+        <Info />
       </div>
     );
   }
