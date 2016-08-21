4f04a87b7cbe3e036fa15d508bf9c397c49dea90
AllenFang <ayu780129@hotmail.com>
2016-08-21 23:42:38 +0800

    project

---
+++ src/components/project.jsx
@@ -0,0 +1,71 @@
+/* eslint arrow-body-style: 0 */
+/* eslint global-require: 0 */
+
+import React from 'react';
+
+require('../styles/project.css');
+
+const PROJECTS = [ {
+  url: 'http://gradusdevelopment.com',
+  className: 'column-center',
+  img: 'react-logo.png',
+  name: 'react'
+}, {
+  url: 'http://andreykeske.com',
+  className: 'column-left',
+  img: 'mobx-logo.png',
+  name: 'Mobx'
+}, {
+  url: 'http://andreykeske.com',
+  className: 'column-right',
+  img: 'webpack-logo.png',
+  name: 'webpack'
+}, {
+  url: 'http://andreykeske.com',
+  className: 'column-center',
+  img: 'react-router-logo.png',
+  name: 'react-router'
+}, {
+  url: 'http://andreykeske.com',
+  className: 'column-left',
+  img: 'babel-logo.png',
+  name: 'babel'
+}, {
+  url: 'http://andreykeske.com',
+  className: 'column-right',
+  img: 'karma-logo.png',
+  name: 'karma'
+} ];
+
+const mapProj = proj =>
+  <div className='project'>
+    <a href={ proj.url }>
+      <img
+        alt={ proj.name }
+        src={ require(`../images/${proj.img}`) } />
+    </a>
+    <ul><li>{ proj.name }</li></ul>
+  </div>;
+
+const Project = () =>
+  <section>
+    <div id='wrapper'>
+      <div className='column-center'>
+        {
+          PROJECTS.filter(proj => proj.className === 'column-center').map(mapProj)
+        }
+      </div>
+      <div className='column-left'>
+        {
+          PROJECTS.filter(proj => proj.className === 'column-left').map(mapProj)
+        }
+      </div>
+      <div className='column-right'>
+        {
+          PROJECTS.filter(proj => proj.className === 'column-right').map(mapProj)
+        }
+      </div>
+    </div>
+  </section>;
+
+export default Project;
