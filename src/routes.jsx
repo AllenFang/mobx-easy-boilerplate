3652385d171c78708bb33d654069ec136b223ede
AllenFang <ayu780129@hotmail.com>
2016-08-21 16:26:32 +0800

    change index route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import AppRoute from './routers/app';
+import HomeRoute from './routers/home';
 import TimerRoute from './routers/timer';
 
 export default (
   <Route path='/' component={ AppRoute }>
-    <IndexRoute component={ TimerRoute } />
+    <IndexRoute component={ HomeRoute } />
+    <Route path='timer' component={ TimerRoute } />
   </Route>
 );
 
