2333e27149ecc77d3ceb1888665f1a4f262642a0
AllenFang <ayu780129@hotmail.com>
2016-08-21 16:26:48 +0800

    footer

---
+++ src/components/shared/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () =>
+  <footer role='contentinfo'>
+    <address>
+      <p>For further information, please contact
+        <a href='mailto:ayu780129@hotmail.com'>AllenFang</a>.
+      </p>
+    </address>
+
+    <small>Copyright &copy; <time dateTime='2016'>2016</time></small>
+  </footer>;
+
+export default Footer;
--- src/routers/app.jsx
+++ src/routers/app.jsx
@@ -3,23 +3,11 @@ import React, { PropTypes } from 'react';
 import { Provider } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
+import Header from '../components/shared/header';
+import Footer from '../components/shared/footer';
 import timerStore from '../stores/timer';
 
-
-// const AppRoute = ({ children }) => {
-//   <div>
-//     <Provider { ...{ timerStore } }>
-//       { children }
-//     </Provider>
-//     <DevTools />
-//   </div>;
-// };
-//
-// AppRoute.propTypes = {
-//   children: PropTypes.any
-// };
-
-// export default AppRoute;
+require('../styles/app.css');
 
 export default class AppRoute extends React.Component {
   static propTypes = {
@@ -29,9 +17,13 @@ export default class AppRoute extends React.Component {
   render() {
     return (
       <div>
-        <Provider { ...{ timerStore } }>
-          { this.props.children }
-        </Provider>
+        <main className='wrap' role='main'>
+          <Header />
+          <Provider { ...{ timerStore } }>
+            { this.props.children }
+          </Provider>
+        </main>
+        <Footer />
         <DevTools />
       </div>
     );
