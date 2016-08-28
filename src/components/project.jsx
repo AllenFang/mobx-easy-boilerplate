cbc06f576178b80cb9e6cfb55c9b8af104161b66
AllenFang <ayu780129@hotmail.com>
2016-08-28 18:56:28 +0800

    update projects list

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,47 +6,47 @@ import React from 'react';
 require('styles/project.css');
 
 const PROJECTS = [ {
-  url: 'http://gradusdevelopment.com',
+  url: 'https://facebook.github.io/react/',
   className: 'column-center',
   img: 'react-logo.png',
-  name: 'react'
+  name: 'react.js'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://mobxjs.github.io/mobx/',
   className: 'column-left',
   img: 'mobx-logo.png',
   name: 'Mobx'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://webpack.github.io/',
   className: 'column-right',
   img: 'webpack-logo.png',
   name: 'webpack'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://github.com/reactjs/react-router',
   className: 'column-center',
   img: 'react-router-logo.png',
   name: 'react-router'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://babeljs.io/',
   className: 'column-left',
   img: 'babel-logo.png',
   name: 'babel'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://karma-runner.github.io/1.0/index.html',
   className: 'column-right',
   img: 'karma-logo.png',
   name: 'karma'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'http://eslint.org/',
   className: 'column-center',
   img: 'eslint-logo.png',
   name: 'eslint'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://mochajs.org/',
   className: 'column-left',
   img: 'mocha-logo.png',
   name: 'mocha'
 }, {
-  url: 'http://andreykeske.com',
+  url: 'https://github.com/gaearon/react-hot-loader',
   className: 'column-right',
   img: 'react-hot-loader-logo.png',
   name: 'react-hot-loader'
@@ -59,7 +59,7 @@ const mapProj = proj =>
         alt={ proj.name }
         src={ require(`../images/${proj.img}`) } />
     </a>
-    <ul><li>{ proj.name }</li></ul>
+    <p className='project-title'>{ proj.name }</p>
   </div>;
 
 const Project = () =>
